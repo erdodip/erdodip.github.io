017e4a7013b0720c6c15fa7eb2c7ed32fee1c244
Peter Erdodi <65724132+erdodip@users.noreply.github.com>
2021-04-17 18:56:19 +0200

    Fix typos

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function dark_theme() {
   document.querySelector('#link_rpg_tools').setAttribute('class','btn btn-dark');
 }
 
-function ligth_theme() {
+function light_theme() {
   // Change theme color of buttons
   document.querySelector('#btn_dark').setAttribute('class','btn btn-sm btn-outline-dark');
   document.querySelector('#btn_light').setAttribute('class','btn btn-sm btn-secondary');
@@ -23,4 +23,4 @@ function ligth_theme() {
 }
 
 document.querySelector('#btn_dark').addEventListener('click',dark_theme);
-document.querySelector('#btn_light').addEventListener('click',ligth_theme);
+document.querySelector('#btn_light').addEventListener('click',light_theme);
