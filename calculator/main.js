830a6e966f1e9bbbdfb5d010fa6843b303e34f64
Peter Erdodi <65724132+erdodip@users.noreply.github.com>
2021-05-14 01:44:27 +0200

    Delete -source-code files and remove minify

--- calculator/main.js
+++ calculator/main.js
@@ -1,1 +1,34 @@
-function check_lcd_value_is_only_zero(){return"0"===LCD.innerHTML}function set_typed_key_to_lcd_value(_){LCD.innerHTML=_}function add_typed_key_to_lcd(_){LCD.innerHTML+=_}function show_typed_key_on_lcd(_){(check_lcd_value_is_only_zero()?set_typed_key_to_lcd_value:add_typed_key_to_lcd)(_)}function show_result_on_lcd(_){set_lcd_value_zero(),LCD.innerHTML=_}function set_lcd_value_zero(){LCD.innerHTML=0}function solve_lcd_value(){show_result_on_lcd(eval(LCD.innerHTML))}LCD=document.querySelector(".calculator-lcd");
+LCD = document.querySelector(".calculator-lcd")
+
+function check_lcd_value_is_only_zero() {
+  return LCD.innerHTML === "0"
+}
+
+function set_typed_key_to_lcd_value(key) {
+  LCD.innerHTML = key
+}
+
+function add_typed_key_to_lcd(key) {
+  LCD.innerHTML += key
+}
+
+function show_typed_key_on_lcd(key) {
+  if (check_lcd_value_is_only_zero()) {
+    set_typed_key_to_lcd_value(key)
+  } else {
+    add_typed_key_to_lcd(key)
+  }
+}
+
+function show_result_on_lcd(result) {
+  set_lcd_value_zero()
+  LCD.innerHTML = result
+}
+
+function set_lcd_value_zero() {
+  LCD.innerHTML = 0
+}
+
+function solve_lcd_value() {
+  show_result_on_lcd(eval(LCD.innerHTML))
+}
